5c4362b96e7055100eafafba7ba1fdb400d53bb4
Amanda Dillon <agdillon@gmail.com>
2018-09-18 13:51:49 -0600

    now you can get questions and click through them

--- scripts.js
+++ scripts.js
@@ -3,6 +3,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
   let display = document.getElementById("display")
 
+  // this function sets up the empty categories board
+  // may need to clear previous display content when coming back to it
   function makeCategoriesModule() {
     let howManyRows = 2
     let rows = []
@@ -10,9 +12,9 @@ document.addEventListener("DOMContentLoaded", () => {
       rows[j] = document.createElement("div")
       rows[j].className = "row"
 
-      for (let i = 1; i <= howManyCategories / howManyRows; i++) {
+      for (let i = 1; i <= howManyCategories / howManyRows; i++) { // 3x2, could add responsiveness here instead
         let catTV = document.createElement("div")
-        catTV.className = "col-md-3 py-4 tv card-front"
+        catTV.className = "col-md-3 py-4 tv category-card"
         if (j === 1) {
           catTV.id = `cat${i}`
         }
@@ -27,73 +29,124 @@ document.addEventListener("DOMContentLoaded", () => {
     rows[1].classList.add("my-1")
   }
 
-  function getCategory(catData) {
-    let id = catData.id
-    let title = catData.title.toUpperCase()
-
-    return {id, title}
-  }
-
+  // main program starts here
   makeCategoriesModule()
 
+  // grab category data, add categories to board, add click listeners
+  //
   axios.get(`http://jservice.io/api/categories?count=6&offset=10`)
     .then((response) => {
       console.log(response.data)
       let cats = []
 
       for (let i = 0; i < response.data.length; i++) {
-        cats[i] = getCategory(response.data[i])
+        let id = response.data[i].id
+        let title = response.data[i].title.toUpperCase()
+        cats[i] = { id, title }
+        let catClickArea = document.getElementById(`cat${i + 1}`)
         let catName = document.getElementById(`cat${i + 1}`).children[0]
         catName.textContent = cats[i].title
+        catClickArea.addEventListener("click", selectCategory)
       }
-
-      display.addEventListener("click", selectCategory)
 
       function selectCategory(event) {
         event.preventDefault()
-        let idNumberString = event.target.parentElement.id.slice(3)
-        let catNo = parseInt(idNumberString) - 1
-        makeQuestionsModule(cats[catNo])
+        let idNumberString = this.id.slice(3)
+        let catNoOnBoard = parseInt(idNumberString, 10) - 1
+        makeQuestionsModule(catNoOnBoard, cats[catNoOnBoard])
       }
 
-      function makeQuestionsModule(category) {
-        // category, dollar value of q
-        // single question in tv box
-        // answer blank and submit button (maybe saying "next question")
+      // this function puts the question into its questionTV div
+      // as well as the header with the category and dollar value
+      function getQuestion(questionNumber, questionsArr, category) {
+        let infoHeader = document.getElementById("info-header")
+        let questionTVText = document.getElementById("question-TV-text")
 
-        // remove categories module and event listener
-        display.removeEventListener("click", selectCategory)
-        while(display.firstChild) {
+        let qValue = questionsArr[questionNumber].value
+        let question = questionsArr[questionNumber].question.toUpperCase()
+
+        infoHeader.textContent = `${category.title} — $${qValue} — ${questionNumber + 1} of 5`
+        questionTVText.textContent = question
+      }
+
+      // this function makes the board setup for the questions
+      // and gets the question data via AJAX, then displays first q
+      function makeQuestionsModule(catNoOnBoard, category) {
+        // remove event listeners
+        for (let i = 0; i < howManyCategories; i++) {
+          let catClickArea = document.getElementById(`cat${i + 1}`)
+          catClickArea.removeEventListener("click", selectCategory)
+        }
+
+        // remove categories module
+        while (display.firstChild) {
           display.removeChild(display.firstChild)
         }
 
-        getQuestion(cats[0].id)
+        axios.get(`http://jservice.io/api/category?id=${category.id}`)
+          .then((qresponse) => {
+            console.log(qresponse.data)
 
-        function getQuestion(id) {
-        // each category on category screen needs to be a link
-        // that link will execute makeQuestionsModule, then this function, loading the
-        // question module with the first question from the
-        // selected category (this is a axios.get request)
+            let questionsArr = qresponse.data.clues
 
-          axios.get(`http://jservice.io/api/category?id=${id}`)
-            .then((qresponse) => {
-              console.log(qresponse.data)
+            // make a space for the category name and dollar value info
+            let infoRow = document.createElement("div")
+            infoRow.classList = "row"
+            let infoCol = document.createElement("div")
+            infoCol.classList = "col-md-6 offset-md-2 text-light text-center"
+            let infoHeader = document.createElement("h2")
+            infoHeader.setAttribute("id", "info-header")
+            infoCol.appendChild(infoHeader)
+            infoRow.appendChild(infoCol)
+            display.appendChild(infoRow)
 
-              let questionTV = document.createElement("div")
-              questionTV.classList = "col-md-4 tv"
-              let questionsArr = qresponse.data.clues
-              questionTV.textContent = questionsArr[0].question // this actually needs to be catNo
+            // make question TV
+            let questionTV = document.createElement("div")
+            // questionTV.setAttribute("id", "question-TV")
+            questionTV.classList = "col-md-4 offset-md-3 tv question-card"
+            let questionTVText = document.createElement("h1")
+            questionTVText.setAttribute("id", "question-TV-text")
+            questionTV.appendChild(questionTVText)
+            let qRow = document.createElement("div")
+            qRow.classList = "row"
+            qRow.appendChild(questionTV)
+            display.appendChild(qRow)
 
-              let row = document.createElement("div")
-              row.classList = "row"
-              let col = document.createElement("div")
-              col.classList = "col-md"
+            // get first question
+            let currentQ = 0
+            getQuestion(currentQ, questionsArr, category)
 
-              col.appendChild(questionTV)
-              row.appendChild(col)
-              display.appendChild(row)
-            })
-        }
+            // make a "next question" button
+            let nextQRow = document.createElement("div")
+            nextQRow.classList = "row"
+            let nextQCol = document.createElement("div")
+            nextQCol.classList = "col-md-4 offset-md-3 text-center pt-2"
+            let nextQButton = document.createElement("button")
+            nextQButton.setAttribute("type", "button")
+            nextQButton.className = "btn btn-light"
+            nextQButton.setAttribute("id", "next-q-button")
+            nextQButton.textContent = "Next question"
+            nextQCol.appendChild(nextQButton)
+            nextQRow.appendChild(nextQCol)
+            display.appendChild(nextQRow)
+
+            function nextQ() {
+              currentQ++
+              getQuestion(currentQ, questionsArr, category)
+
+              // need to remove nextQButton when on last question
+              if (currentQ === 4) {
+                nextQButton.removeEventListener("click", nextQ)
+                nextQButton.parentElement.removeChild(nextQButton)
+              }
+            }
+
+            nextQButton.addEventListener("click", nextQ)
+
+            // answer blank and submit button
+
+
+          })
       }
     })
 })
